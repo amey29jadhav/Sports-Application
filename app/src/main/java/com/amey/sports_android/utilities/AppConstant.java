7f04c9d445e1dee97b122920b2c8112e3367150b
amey29jadhav <amey29jadhav@gmail.com>
2020-04-17 12:39:48 -0400

    Added Constant file

---
+++ app/src/main/java/com/amey/sports_android/utilities/AppConstant.java
@@ -0,0 +1,24 @@
+package com.amey.sports_android.utilities;
+
+public class AppConstant {
+
+    public static final String API_KEY ="1";
+    public static final String API_HOST_NAME ="https://www.thesportsdb.com";
+    public static final String PATH_NAME ="api/v1/json";
+
+
+    // Search
+    public static final String SEARCH_TEAMS ="searchteams.php";
+    public static final String SEARCH_PLAYERS ="searchplayers.php";
+    public static final String SEARCH_EVENTS ="Searchevents.php";
+
+    public static final String GET_ALL_SPORTS ="all_sports.php";
+    public static final String GET_ALL_LEAGUES ="all_leagues.php";
+    public static final String SEARCH_ALL_LEAGUES ="search_all_leagues.php";
+    public static final String SEARCH_ALL_SESSION ="search_all_seasons.php";
+    public static final String SEARCH_ALL_TEAMS ="search_all_teams.php";
+
+
+
+
+}
